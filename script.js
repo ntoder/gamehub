0f59022566cf56c03629fb1d0bac29214215bba9
Noam Toder Yoga <noam@rbscode.com>
2026-04-03 01:37:01 -0700

    fix 	modified:   script.js

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ let mywebsite=document.getElementById("my-website")
 let password = prompt("Please enter your password:")
 if (password === "1234") {
    mywebsite.style.display="block"
-    let firstTime = confirm("Is this your first time here?")
-    if (firstTime) {
-        alert("Welcome! Enjoy your stay.")
-   }    
+     
     alert("Welcome back! You have successfully logged in. Enjoy your stay.")
 } else {
     alert("Wrong password")
